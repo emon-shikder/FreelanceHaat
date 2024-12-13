2dd2addbc065c22aec39ec41a6a8e850f91bb19c
Emon Shikder <codingwithemon@gmail.com>
2024-12-14 02:16:37 +0600

    Updated Python version and fixed compatibility issues

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Run migrations (if necessary)
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
